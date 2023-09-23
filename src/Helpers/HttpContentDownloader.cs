2f4d2597987dfb406eb385e14f30a3aeaa8fb69a
Dylan Morley <dylanm@asos.com>
2023-09-23 19:34:09 +0100

    add HttpContentDownloader class

---
+++ src/Helpers/HttpContentDownloader.cs
@@ -0,0 +1,12 @@
+﻿namespace AzureNamingTool.Helpers;
+
+public class HttpContentDownloader
+{
+    public async Task<string> DownloadString(string url)
+    {
+        HttpClient httpClient = new();
+        var data = await httpClient.GetStringAsync(url);
+
+        return data;
+    }
+}
